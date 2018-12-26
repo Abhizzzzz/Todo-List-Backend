e717c44fabc7eeb1b3de388788808a8addb0cad2
Abhizzzzz <palwankar786@2gmail.com>
2018-12-26 15:25:26 +0530

    First Commit

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Real Time To Do List",
+  "version": "0.0.1",
+  "description": "Documentation for the real time to do list",
+  "title": "Real Time To Do List API's",
+  "url": "http://localhost:3002",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-12-26T09:23:51.393Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
